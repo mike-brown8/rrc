75ba3d0d01197437db71dc0b062c4aac5a9008f9
Mike Brown <nohuitou@outlook.com>
2024-05-03 13:18:02 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm796849995-b2c2db5cca4c6213c2dacdee',
+    'm796849987-47f88e0a43e85cc3ea6e477f',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+	
+    }
+  ],
+};
